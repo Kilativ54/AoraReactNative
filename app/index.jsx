484e45645d80d62ddace2d472ae0418f6db23a01
Kilativ54 <vitalikechkalov@gmail.com>
2024-07-29 11:58:13 +0200

    add tailwind

--- app/index.jsx
+++ app/index.jsx
@@ -1,22 +1,15 @@
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-          <StatusBar style="auto" />
-          <Link href="/profile" style={{ color: "blue" }}>Profile</Link>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-bold">Aora</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" className="text-3xl font-bold color-blue">
+        Profile
+      </Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
