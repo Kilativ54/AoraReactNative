a8c0de1c1247cf7b3f322079bd90cf7d193beb73
Kilativ54 <vitalikechkalov@gmail.com>
2024-08-01 21:11:16 +0200

    add images and text to index and creat customButton

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,45 @@
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
+import { Image } from "react-native";
+import  CustomButton  from "../components/CustomButton";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Aora</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" className="text-3xl font-bold color-blue">
-        Home
-      </Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full justify-center items-center h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[300px] max-h-[300px]"
+          />
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless Possibilities with{" "}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              className="absolute -bottom-2 -right-8 w-[136px] h-[15px]"
+              resizeMode="contain"
+            />
+          </View>
+
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where creativity meets innovation: embark on a journey of limitless
+            exploration with Aora
+          </Text>
+          <CustomButton />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
