6952c4080ac1d439363c42a04fe64b29bad7d671
Kilativ54 <vitalikechkalov@gmail.com>
2024-08-04 21:33:19 +0200

    add content and styles to FormField

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -2,31 +2,44 @@ import { View, Text } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ScrollView } from "react-native";
-import { images } from '../../constants'
+import { images } from "../../constants";
 import { Image } from "react-native";
 import FormField from "../../components/FormField";
-import { useState } from 'react' 
+import { useState } from "react";
 
 const SignIn = () => {
   const [form, setForm] = useState({
-    email: '',
-    password: '',
-})
+    email: "",
+    password: "",
+  });
 
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
         <View className="h-full w-full justify-center px-4 my-6">
-<Image source={images.logo} resizeMode="contain" className="w-[115px] h-[35px]"/>
-          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">Log in to Aora</Text>
-          
-          <FormField title="Email"
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[35px]"
+          />
+          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">
+            Log in to Aora
+          </Text>
+
+          <FormField
+            title="Email"
             value={form.email}
             handleChangeText={(e) => setForm({ ...form, email: e })}
             otherStyles="mt-7"
             keyboardType="email-address"
           />
 
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
