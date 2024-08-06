bed2eec32202ce1ae4a2b29598bc0cd53977af47
Kilativ54 <vitalikechkalov@gmail.com>
2024-08-06 10:15:34 +0200

    add button submit to sign in

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -6,12 +6,17 @@ import { images } from "../../constants";
 import { Image } from "react-native";
 import FormField from "../../components/FormField";
 import { useState } from "react";
+import CustomButton from "../../components/CustomButton";
+import { Link } from "expo-router";
+
 
 const SignIn = () => {
   const [form, setForm] = useState({
     email: "",
     password: "",
   });
+
+  const [isSubmitting, setisSubmitting] = useState(false)
 
   return (
     <SafeAreaView className="bg-primary h-full">
@@ -40,6 +45,23 @@ const SignIn = () => {
             handleChangeText={(e) => setForm({ ...form, email: e })}
             otherStyles="mt-7"
           />
+          <CustomButton
+            title="Sign In"
+            handlePress
+            containerStyle="mt-7"
+            isLoading={isSubmitting}
+          />
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Don't have an account?
+            </Text>
+            <Link
+              href="/sign-up"
+              className="text-lg font-psemibold text-secondary"
+            >
+              Sign Up
+            </Link>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
