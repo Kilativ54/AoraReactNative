883d82ac574bf7bd37660e11a418d0a4056f6aaf
Kilativ54 <vitalikechkalov@gmail.com>
2024-08-14 22:45:21 +0200

    add posobility to play video on home page

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -10,6 +10,7 @@ import React from "react";
 import * as Animatable from "react-native-animatable";
 import { useState } from "react";
 import { icons } from "../constants";
+import { Video, ResizeMode } from "expo-av";
 
 const zoomIn = {
   0: {
@@ -38,7 +39,16 @@ const TrendingItem = ({ activeItem, item }) => {
       duration={500}
     >
       {play ? (
-        <Text className="text-white">Playing</Text>
+        <Video
+          source={{ uri: item.video }}
+          className="w-52 h-72 rounded-[35px] mt-3 bg-white/10"
+          resizeMode={ResizeMode.CONTAIN}
+          useNativeControls
+          shouldPlay
+          onPlaybackStatusUpdate={(status) => {
+            if (status.didJustFinish) {setPlay(false)};
+          }}
+        />
       ) : (
         <TouchableOpacity
           className="relative justify-center items-center"
@@ -62,13 +72,13 @@ const TrendingItem = ({ activeItem, item }) => {
   );
 };
 
- const Trending = ({ posts }) => {
+const Trending = ({ posts }) => {
   const [activeItem, setActiveItem] = useState(posts[1]);
-   const viewableItemsChanged = ({ viewableItems }) => {
-     if (viewableItems.length > 0) {
-       setActiveItem(viewableItems[0].key);
-     }
-   }
+  const viewableItemsChanged = ({ viewableItems }) => {
+    if (viewableItems.length > 0) {
+      setActiveItem(viewableItems[0].key);
+    }
+  };
   return (
     <FlatList
       data={posts}
@@ -78,7 +88,7 @@ const TrendingItem = ({ activeItem, item }) => {
       )}
       onViewableItemsChanged={viewableItemsChanged}
       viewabilityConfig={{
-        itemVisiblePercentThreshold: 70
+        itemVisiblePercentThreshold: 70,
       }}
       contentOffset={{ x: 170 }}
       horizontal
--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -2,6 +2,7 @@ import { View, Text, Image, TouchableOpacity } from "react-native";
 import React from "react";
 import { icons } from "../constants";
 import { useState } from "react";
+import { Video, ResizeMode } from "expo-av";
 
 const VideoCard = ({ video: { title, thumbnail, video, avatar } }) => {
   const [play, setPlay] = useState(false);
@@ -34,7 +35,18 @@ const VideoCard = ({ video: { title, thumbnail, video, avatar } }) => {
       </View>
 
       {play ? (
-        <Text className="text-white font-psemibold">Playing</Text>
+        <Video
+          source={{ uri: video }}
+          className="w-full h-60 rounded-xl mt-3 "
+          resizeMode={ResizeMode.CONTAIN}
+          useNativeControls
+          shouldPlay
+          onPlaybackStatusUpdate={(status) => {
+            if (status.didJustFinish) {
+              setPlay(false);
+            }
+          }}
+        />
       ) : (
         <TouchableOpacity
           activeOpacity={0.7}
