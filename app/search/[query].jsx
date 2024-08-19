ca9f4a48ce5d9f0f3a560963508517070199c54e
Kilativ54 <vitalikechkalov@gmail.com>
2024-08-19 23:55:31 +0200

    add content to search query

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,13 +1,62 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import { useLocalSearchParams } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { searchPosts } from "../../lib/appwrite";
+import { getLatestPosts } from "../../lib/appwrite";
+import { FlatList } from "react-native";
+import VideoCard from "../../components/VideoCard";
+import useAppwrite from "../../lib/useAppwrite";
+import { RefreshControl } from "react-native";
+import { useState } from "react";
+import { Alert } from "react-native";
+import { getAllPosts } from "../../lib/appwrite";
 
 const Search = () => {
   const { query } = useLocalSearchParams();
+  const { data: posts, refetch } = useAppwrite(searchPosts(query));
+  const { data: latestPosts } = useAppwrite(getLatestPosts);
+
+  useEffect(() => {
+    refetch();
+  }, [query]);
+
   return (
     <SafeAreaView className="bg-primary h-full">
-      <Text className="text-3xl text-white">{query}</Text>
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => <VideoCard video={item} />}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4 space-y-6">
+            <View className="justify-between items-start flex-row mb-6">
+              <View>
+                <Text className="font-pmedium text-sm text-gray-100">
+                  Welcome Back
+                </Text>
+                <Text className="text-2xl font-psemibold text-white">
+                  Vitalii
+                </Text>
+              </View>
+              <View className="mt-1.5">
+                <Image
+                  source={images.logoSmall}
+                  className="w-9 h-10"
+                  resizeMode="contain"
+                />
+              </View>
+            </View>
+
+            <SearchInput />
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subtitle="Be the first one to upload a video"
+          />
+        )}
+      />
     </SafeAreaView>
   );
 };
